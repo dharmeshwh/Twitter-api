f6309108d99025bd88180c1f187388d726f459d5
dharmeshwh <dharmeshaheer@gmail.com>
2023-12-23 14:41:40 +0530

    tested all routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import apiRoutes from "./routes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
@@ -8,6 +9,13 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser("mySecret"));
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
 
 // Route handling
 app.use("/", apiRoutes);
